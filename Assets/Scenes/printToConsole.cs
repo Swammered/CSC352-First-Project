d2cd935bb29b423ce0b8b5fa297d9fdb5624e01d
Swammered <jamesswim12@yahoo.com>
2022-11-04 13:46:01 -0400

    fixed transform.translate

--- Assets/Scenes/printToConsole.cs
+++ Assets/Scenes/printToConsole.cs
@@ -13,7 +13,7 @@ public class printToConsole : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0,0,0.01f);
-        transform.Translate(0,0.01f,0);
+        transform.Rotate(0,0,0.1f);
+        transform.Translate(0.01f,0,0);
     }
 }
